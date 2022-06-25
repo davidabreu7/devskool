683dda468b742392412cc6e1b799d35709fe16fe
David Abreu <david.abreu@mpdft.mp.br>
2022-06-25 14:18:37 -0300

    Initial commit

---
+++ src/test/java/com/devsuperior/dslearn/DslearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dslearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
